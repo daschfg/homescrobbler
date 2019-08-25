aa162fe1bfcb0246478f669d01c135a6c86d22f2
Daniel Schubert <daschfg@posteo.de>
2019-08-25 23:34:35 +0200

    Config-Vorlage hinzu

---
+++ config.py
@@ -0,0 +1,10 @@
+volumio_address = ('192.168.2.104', 3000)
+raumfeld_address = ('192.168.2.126', 8080)
+
+API_KEY = 'YOUR_API_KEY'
+API_SECRET = 'YOUR_API_SECRET'
+
+lastfm_username = 'YOUR_USERNAME'
+lastfm_password_hash = 'YOUR_PASSWORD'
+
+dbfile="musiclog.db"
