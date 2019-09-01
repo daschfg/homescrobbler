9425971460289944a5ecb904d017e6e4f49e3d38
Daniel Schubert <daschfg@posteo.de>
2019-09-01 15:02:42 +0200

    GNU GPLv3 als Lizenz hinzu

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='homescrobbler',
-        version='1.0.0',
-        packages=find_packages()
-    )
+    name='homescrobbler',
+    version='1.0.0',
+    license='GPLv3+',
+    packages=find_packages()
+)
