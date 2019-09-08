14a974906e2175afd276faa6390ee6796aa90492
Daniel Schubert <daschfg@posteo.de>
2019-09-08 14:00:38 +0200

    Versionsnummer und Programmname auch für setup.py zentral hinterlegt

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from homescrobbler import __progname__, __version__
 
 setup(
-    name='homescrobbler',
-    version='1.0.0',
+    name=__progname__,
+    version=__version__,
     license='GPLv3+',
     packages=find_packages()
 )
